a776fda0c3ecded87fbd3e08e67157ced9ba3dab
Patrik Zelena <zpatrik94@gmail.com>
2019-07-21 21:01:52 +0200

    Base time seek bar init

--- app/src/main/java/hu/zeletrik/dsmtimer/MainActivity.kt
+++ app/src/main/java/hu/zeletrik/dsmtimer/MainActivity.kt
@@ -1,12 +1,38 @@
 package hu.zeletrik.dsmtimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpleSeekBar = findViewById<SeekBar>(R.id.baseTimeSeekBar)
+        val baseTimeText = findViewById<TextView>(R.id.baseTimeValue)
+
+        simpleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            var progressChangedValue = 0
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                progressChangedValue = progress
+                baseTimeText.text = String.format(getString(R.string.base_time_value), progressChangedValue)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(
+                    this@MainActivity, "Seek bar progress is :$progressChangedValue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
     }
 }
