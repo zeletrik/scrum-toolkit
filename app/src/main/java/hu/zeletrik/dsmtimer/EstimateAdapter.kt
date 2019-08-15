dc15910147492f26b977e210494e2a156eedcc15
Patrik Zelena <zpatrik94@gmail.com>
2019-08-15 20:50:25 +0200

    Implemented estimation cards activity

--- app/src/main/java/hu/zeletrik/dsmtimer/EstimateAdapter.kt
+++ app/src/main/java/hu/zeletrik/dsmtimer/EstimateAdapter.kt
@@ -1,14 +1,23 @@
 package hu.zeletrik.dsmtimer
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import hu.zeletrik.dsmtimer.presenter.activity.EstimationCardActivity
 import kotlinx.android.synthetic.main.estimate_card.view.*
+import androidx.cardview.widget.CardView
+
 
 class EstimateAdapter(private val items: MutableList<String>) : RecyclerView.Adapter<EstimateAdapter.VH>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        return VH(parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.estimate_card, parent, false)
+        return VH(view)
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
@@ -17,16 +26,22 @@ class EstimateAdapter(private val items: MutableList<String>) : RecyclerView.Ada
 
     override fun getItemCount(): Int = items.size
 
-    fun addItem(value: String) {
-        items.add(value)
-        notifyItemInserted(items.size)
-    }
-    
-    class VH(parent: ViewGroup) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.estimate_card, parent, false)) {
+    class VH(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val item = itemView.findViewById<View>(R.id.estimateCard) as TextView
 
-        fun bind(value: String) = with(itemView) {
-            estimateCard.text = value
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(value: String) {
+            item.text = value
+        }
+
+        override fun onClick(view: View) {
+            val context = itemView.context
+            val intent = Intent(context, EstimationCardActivity::class.java)
+            intent.putExtra("value", item.text.toString())
+            context.startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/hu/zeletrik/dsmtimer/presenter/activity/EstimationCardActivity.kt
@@ -0,0 +1,59 @@
+package hu.zeletrik.dsmtimer.presenter.activity
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import hu.zeletrik.dsmtimer.R
+
+class EstimationCardActivity : AppCompatActivity() {
+
+    private lateinit var valueText: TextView
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estiamtioncard)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        valueText = findViewById(R.id.estimationCardValue)
+
+        val value = intent.getStringExtra("value")
+
+        when (value) {
+            "None" -> valueText.setBackgroundColor(getColor(R.color.risk_none))
+            "Low" -> valueText.setBackgroundColor(getColor(R.color.risk_low))
+            "Medium" -> valueText.setBackgroundColor(getColor(R.color.risk_medium))
+            "High" -> valueText.setBackgroundColor(getColor(R.color.risk_high))
+            "Very high" -> valueText.setBackgroundColor(getColor(R.color.risk_veryHigh))
+            else -> {
+                valueText.text = value
+            }
+        }
+
+
+        findViewById<View>(R.id.estimationCardValueHolder).setOnClickListener {
+            if (valueText.visibility == View.VISIBLE) finish()
+            valueText.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            finish()
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+    }
+
+}
